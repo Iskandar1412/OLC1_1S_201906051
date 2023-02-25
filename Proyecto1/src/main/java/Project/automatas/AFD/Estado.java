c92dbfb714f744a3b58c5f9332844537f75f7227
Paco <silvapaco810@gmail.com>
2023-02-24 22:45:22 -0600

    Nuevas Clases Wuuuuu

---
+++ Proyecto1/src/main/java/Project/automatas/AFD/Estado.java
@@ -0,0 +1,17 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project.automatas.AFD;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.UUID;
+/**
+ *
+ * @author Pacos
+ */
+public class Estado {
+    
+}
