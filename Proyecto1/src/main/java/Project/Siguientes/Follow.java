6259ecf1f8c8c22c92f82a38f55b891169b48cf7
Paco <silvapaco810@gmail.com>
2023-02-26 19:29:50 -0600

    Ajustes

--- Proyecto1/src/main/java/Project/Siguientes/Follow.java
+++ Proyecto1/src/main/java/Project/Siguientes/Follow.java
@@ -3,10 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Project.Siguientes;
-//import java.io.FileWriter;
-//import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
-///import java.util.UUID;
+import java.util.UUID;
 /**
  *
  * @author Pacos
@@ -18,11 +18,55 @@ public class Follow {
     //public ArrayList<String> NoTerminal;
     //public ArrayList<String> Siguientes;
     //public int ID;
+    public String ID;
+    public ArrayList<Siguiente> Siguiente;
     
-    public Follow() {
+    
+    public Follow(String id, ArrayList<Siguiente> siguiente) {
         //this.Terminal = new ArrayList<>();
         //this.Hoja = "";
         //this.ID = 0;
         //this.Siguientes = new ArrayList<>();
+        this.ID = id;
+        this.Siguiente = siguiente;
+    }
+    
+    public String getdot() {
+        String dot = "digraph G{ \n\trankdir=LR;\n\tnode[shape=record];\n";
+        dot += "a[label=\"{Hoja|Terminal|Siguiente}";
+        
+        
+        return dot;
+    }
+    
+    public void write(String path, String content) {
+        FileWriter fichero = null;
+        PrintWriter p = null;
+        try{
+            fichero = new FileWriter(path);
+            p = new PrintWriter(fichero);
+            p.close();
+            fichero.close();
+        }catch(Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }finally{
+            if(p != null) {
+                p.close();
+            }
+        }
+    }
+    
+    public void Dot() {
+        try{
+            String path = this.ID + ".txt";
+            String name = this.ID + ".svg";
+            write(path, getdot());
+            ProcessBuilder tosvg;
+            tosvg = new ProcessBuilder("dot", "-Tsvg", "-o", name, path);
+            tosvg.redirectErrorStream(true);
+            tosvg.start();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
