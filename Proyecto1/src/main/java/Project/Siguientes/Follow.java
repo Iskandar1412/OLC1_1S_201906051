50bfd661ebd83c3b25bea0eac125a4cf0398f687
Paco <silvapaco810@gmail.com>
2023-02-26 19:20:24 -0600

    Nuevas Clases

---
+++ Proyecto1/src/main/java/Project/Siguientes/Follow.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Project.Siguientes;
+//import java.io.FileWriter;
+//import java.io.PrintWriter;
+import java.util.ArrayList;
+///import java.util.UUID;
+/**
+ *
+ * @author Pacos
+ */
+public class Follow {
+    public String Symbol;
+    public String Hoja;
+    public ArrayList<String> Terminal;
+    
+    public Follow() {
+        this.Terminal = new ArrayList<>();
+        this.Hoja = "";
+    }
+}
