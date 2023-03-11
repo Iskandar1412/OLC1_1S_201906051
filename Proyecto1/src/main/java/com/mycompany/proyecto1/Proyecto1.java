9934661baff13bf03e00180b5a472927ddbd350f
Paco <silvapaco810@gmail.com>
2023-03-10 19:37:17 -0600

    Arreglos

--- Proyecto1/src/main/java/com/mycompany/proyecto1/Proyecto1.java
+++ Proyecto1/src/main/java/com/mycompany/proyecto1/Proyecto1.java
@@ -1,10 +1,7 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.proyecto1;
-import Project.errors.Errores;
-
 /**
  *
  * @author Pacos
@@ -14,6 +11,8 @@ public class Proyecto1 {
     public static void main(String[] args) {
         //System.out.println("Hello World!");
         //ProyectoGrafica graph = new ProyectoGrafica();
-        
+        Grafica graf = new Grafica();
+        graf.setVisible(true);
+        graf.setLocationRelativeTo(null);
     }
 }
